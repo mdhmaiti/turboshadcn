e69820ab3ecb71de6683efb813571d9b5c87d020
Medhashis_Maiti <medhashis000@gmail.com>
2024-06-28 23:47:54 +0530

    init

---
+++ apps/my-app/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    transpilePackages:["@repo/uinext"],
+ 
+  
+};
+
+export default nextConfig;
---
+++ apps/my-app/src/app/page.tsx
@@ -0,0 +1,19 @@
+
+
+
+import { Button } from "@repo/uinext/components/ui/button";
+import { Input } from "@repo/uinext/components/ui/input";
+
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>hello lawra</Button>
+      <Input/>
+      
+      
+    </main>
+  );
+}
